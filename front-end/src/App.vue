be6127aa090e5b03ff23448d66c600e2b9197f33
NodeWillDev <williamdasilva.dev@gmail.com>
2025-03-12 09:13:14 -0300

    91° commit from 2025

---
+++ front-end/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <h2>Fala poha</h2>
+    </div>
+  </header>  
+</template>
