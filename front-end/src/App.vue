e5a05b33b4d56bfccf64fc1173c338ad562c93f8
NodeWillDev <williamdasilva.dev@gmail.com>
2025-03-18 13:18:52 -0300

    103° commit from 2025

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import MainSearchComponents from './components/MainSearchComponents.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 </script>
 
 <template>
     <div class="w-screen h-screen flex items-center">
-        <MainSearchComponents />
+        <MainSearchComponents :icon="['fas', 'magnifying-glass']" />
     </div>
 </template>
--- front-end/src/components/MainSearchComponents.vue
+++ front-end/src/components/MainSearchComponents.vue
@@ -1,27 +1,36 @@
 <template>
 
-    <div class="flex flex-wrap absolute w-full top-72">
+    <div class="group flex flex-wrap absolute w-full top-72">
         <div class="flex w-full gap-3.5">
-            <font-awesome-icon icon="magnifying-glass" class="text-5xl text-emerald-600 duration-300 relative m-auto" />
+            <font-awesome-icon :icon="['fas', 'magnifying-glass']"
+                class="text-5xl text-emerald-600 duration-300 relative m-auto" />
             <input
                 class="outline-0 focus:border-b-emerald-300 p-6 w-full text-4xl duration-300 border-b-1 border-b-emerald-600 focus:bg-[#D1E3F0]"
-                type="text" @input="changeEnvet">
+                type="text" v-model="name">
         </div>
         <div class="w-full max-h-[50vh] overflow-auto p-3 mt-3.5">
-            <div v-for="user in data"
-                class="flex mt-2 bg-[#333333] p-2 items-center gap-4 hover:opacity-75 duration-700">
-                <img src="../assets/unknow.jpg" alt="?" srcset="" class="w-40 h-40 rounded-full">
-                <div class="flex flex-col gap-1.5">
-                    <h3 class="text-[24px] text-emerald-500">Name: {{ user.name }}</h3>
-                    <h3 class="text-[24px] text-emerald-100"><font-awesome-icon icon="id-badge"
-                            class="mr-2 opacity-55" /> {{
-                                user.id }}</h3>
-                    <h3 class="text-[20px] text-emerald-100"><font-awesome-icon icon="heart" class="mr-2 opacity-55" />
-                        {{ user.age
-                        }}</h3>
-                    <h3 class="text-[20px] text-emerald-100"><font-awesome-icon icon="book" class="mr-2 opacity-55" />{{
-                        user.description }}
-                    </h3>
+            <div v-show="data.length > 0" v-for="user in data"
+                class="hidden mt-2 bg-[#333333] p-2 items-center gap-4 hover:opacity-75 duration-700 group-focus-within:flex justify-between ">
+                <div class="flex gap-x-6">
+                    <img src="../assets/unknow.jpg" alt="?" srcset="" class="w-40 h-40 rounded-full">
+                    <div class="flex flex-col gap-1.5">
+                        <h3 class="text-[24px] text-emerald-500">Name: {{ user.name }}</h3>
+                        <h3 class="text-[24px] text-emerald-100"><font-awesome-icon icon="id-badge"
+                                class="mr-2 opacity-55" /> {{
+                                    user.id }}</h3>
+                        <h3 class="text-[20px] text-emerald-100"><font-awesome-icon icon="heart"
+                                class="mr-2 opacity-55" />
+                            {{ user.age
+                            }}</h3>
+                        <h3 class="text-[20px] text-emerald-100"><font-awesome-icon :icon="['fas', 'book']"
+                                class="mr-2 opacity-55" />{{
+                                    user.description }}
+                        </h3>
+                    </div>
+                </div>
+                <div class="flex gap-x-3.5">
+                    <button class="bg-violet-400 p-2 rounded-[10px] text-white font-medium tracking-wider">Edit</button>
+                    <button>Delete</button>
                 </div>
             </div>
         </div>
@@ -30,10 +39,16 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { UserInterface } from '@user/UserInterface';
-
-const data = ref<UserInterface[]>([
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+const name = ref<String>('')
+const data = ref<UserInterface[]>([])
+watch(name, () => {
+    data.value = api.filter(user => user.name.toLowerCase().includes(String(name.value.toLowerCase())))
+    console.log(data)
+})
+const api: UserInterface[] = ([
     {
         id: 1,
         name: "William da Silva",
@@ -58,7 +73,9 @@ const data = ref<UserInterface[]>([
 ])
 
 function changeEnvet(event: Event) {
-    console.log(event)
-    data.value = data.value.filter(user => user.name.toLowerCase() == (event.target as HTMLInputElement).value.toLowerCase())
+
+    // data.value = [data.value[data.value.findIndex(user => user.name.toLowerCase() == (event.target as HTMLInputElement).value.toLowerCase())]]
+    // console.log(data.value)
 }
+
 </script>
